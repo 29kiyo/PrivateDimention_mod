57e07b5613d1efc107db3448e283d4ee5a5ab986
29kiyo <148545965+29kiyo@users.noreply.github.com>
2026-06-12 08:12:55 +0000

    fix: neoforge-12111/2611/2612 Commands.hasPermission(2)スタティックメソッドに変更

--- neoforge-12111/src/main/java/dev/keiragi/privatedimension/neoforge/NeoForgeCommandHandler.java
+++ neoforge-12111/src/main/java/dev/keiragi/privatedimension/neoforge/NeoForgeCommandHandler.java
@@ -20,12 +20,12 @@ public class NeoForgeCommandHandler {
                          CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(Commands.literal("pd")
             .then(Commands.literal("give")
-                .requires(src -> src.hasPermission(2))
+                .requires(Commands.hasPermission(2))
                 .executes(ctx -> giveSelf(ctx, mod))
                 .then(Commands.argument("player", StringArgumentType.word())
                     .executes(ctx -> givePlayer(ctx, mod))))
             .then(Commands.literal("reload")
-                .requires(src -> src.hasPermission(2))
+                .requires(Commands.hasPermission(2))
                 .executes(ctx -> reload(ctx, mod)))
             .then(Commands.literal("info")
                 .executes(ctx -> info(ctx, mod))));
