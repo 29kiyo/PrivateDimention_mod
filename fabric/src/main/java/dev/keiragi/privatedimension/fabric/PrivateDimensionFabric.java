68bf19c7c0a0647f5bdf0f2a71a4e5b6099da05e
29kiyo <148545965+29kiyo@users.noreply.github.com>
2026-06-13 13:55:58 +0000

    debug: アイテム登録ログ追加

--- fabric/src/main/java/dev/keiragi/privatedimension/fabric/PrivateDimensionFabric.java
+++ fabric/src/main/java/dev/keiragi/privatedimension/fabric/PrivateDimensionFabric.java
@@ -31,11 +31,13 @@ public class PrivateDimensionFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         // アイテム登録
+        PrivateDimensionMod.LOGGER.info("アイテム登録開始");
         Registry.register(
             BuiltInRegistries.ITEM,
             ResourceLocation.fromNamespaceAndPath("privatedimension", "dimension_bottle"),
             ModItems.createDimensionBottle()
         );
+        PrivateDimensionMod.LOGGER.info("アイテム登録完了: {}", ModItems.DIMENSION_BOTTLE);
 
         mod = new PrivateDimensionMod();
         mod.init();
